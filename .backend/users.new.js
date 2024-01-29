f78a3f062dca9a628db2d800fbaf4966f6cb9a1c
Laurent <laurent.cuadras@gmail.com>
2024-01-29 21:45:58 +0100

    Initial commit

---
+++ .backend/users.new.js
@@ -0,0 +1,13 @@
+module.exports = [
+    {
+        id: 1,
+        firstName: "Terry",
+        lastName: "Medhurst",
+        gender: "male",
+        email: "olivier@mail.com",
+        phone: "+63 791 675 8914",
+        password: "$2a$10$dTjT0UT3WER2y7J7kpz4yexvjJMrW3jQgGkwW.hu1vnn59KVW3VUG",
+        image: "https://robohash.org/hicveldicta.png",
+        admin: true
+    }
+]
